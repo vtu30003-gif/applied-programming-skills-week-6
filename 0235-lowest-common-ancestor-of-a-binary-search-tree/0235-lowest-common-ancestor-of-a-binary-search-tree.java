a1cede09aceb42ac11f2cc00375ec58e570f5be2
vtu30003-gif <vtu30003@veltech.edu.in>
2026-09-22 14:33:32 +0530

    Time: 6 ms (97.05%), Space: 47.9 MB (17.18%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // If both p and q are smaller than root, LCA is in the left subtree
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        
+        // If both p and q are greater than root, LCA is in the right subtree
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        
+        // We have found the split point, which is the LCA
+        return root;
+    }
+}
